9de73c296e4c197e0e05f18822e0cccf957fe423
mark janssen <mark.janssen-vooles@xero.com>
2022-03-19 14:27:26 +1100

    Change content

--- src/App.js
+++ src/App.js
@@ -5,18 +5,13 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Mark Janssen Portfolio
+
+        Inspired by: https://roadmap.sh/frontend
+        https://github.com/Mark-Cooper-Janssen-Vooles/frontend-webdev-guide
+        https://github.com/Mark-Cooper-Janssen-Vooles/backend-webdev-guide
+        https://github.com/Mark-Cooper-Janssen-Vooles/devops-webdev-guide
+
       </header>
     </div>
   );
