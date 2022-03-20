5c11d69fa74aa990774edbf721b6d8e6220e6475
mark janssen <mark.janssen-vooles@xero.com>
2022-03-21 09:22:27 +1100

    Fill out some content

--- src/App.js
+++ src/App.js
@@ -4,13 +4,16 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        Mark Janssen Portfolio
-
-        Inspired by: https://roadmap.sh/frontend
-        https://github.com/Mark-Cooper-Janssen-Vooles/frontend-webdev-guide
-        https://github.com/Mark-Cooper-Janssen-Vooles/backend-webdev-guide
-        https://github.com/Mark-Cooper-Janssen-Vooles/devops-webdev-guide
-
+        <h1>Mark Janssen Portfolio</h1>
+        <div>
+          I'm an architectural graduate turned full stack web software developer, predominantly using Javascript/React, C#/.net and AWS.
+          This space is to detail my ongoing learnings, predominantly inspired by https://roadmap.sh/: 
+        </div>
+        <ul>
+          <li>https://github.com/Mark-Cooper-Janssen-Vooles/frontend-webdev-guide</li>
+          <li>https://github.com/Mark-Cooper-Janssen-Vooles/backend-webdev-guide</li>
+          <li>https://github.com/Mark-Cooper-Janssen-Vooles/devops-webdev-guide</li>
+        </ul>
       </header>
     </div>
   );
