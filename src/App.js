fa5144dc4701c62d8cfacec004a65a53ef0892b9
mark janssen <mark.janssen-vooles@xero.com>
2022-03-21 09:28:08 +1100

    Add style

--- src/App.js
+++ src/App.js
@@ -4,22 +4,24 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Mark Janssen Portfolio</h1>
-        <div>
-          I'm an architectural graduate turned full stack web software developer, predominantly using Javascript/React, C#/.net and AWS.
-          This space is to detail my ongoing learnings, predominantly inspired by https://roadmap.sh/: 
+        <div className="Content-wrap">
+          <h1>Mark Janssen Portfolio</h1>
+          <div>
+            I'm an architectural graduate turned full stack web software developer, predominantly using Javascript/React, C#/.net and AWS.
+            This space is to detail my ongoing learnings, predominantly inspired by https://roadmap.sh/
+          </div>
+          <ul>
+            <li>
+              <a href="https://github.com/Mark-Cooper-Janssen-Vooles/frontend-webdev-guide">frontend-webdev-guide</a>
+            </li>
+            <li>
+              <a href="https://github.com/Mark-Cooper-Janssen-Vooles/backend-webdev-guide">backend-webdev-guide</a>
+            </li>
+            <li>
+              <a href="https://github.com/Mark-Cooper-Janssen-Vooles/devops-webdev-guide">devops-webdev-guide</a>
+            </li>
+          </ul>
         </div>
-        <ul>
-          <li>
-            <a href="https://github.com/Mark-Cooper-Janssen-Vooles/frontend-webdev-guide">frontend-webdev-guide</a>
-          </li>
-          <li>
-            <a href="https://github.com/Mark-Cooper-Janssen-Vooles/backend-webdev-guide">backend-webdev-guide</a>
-          </li>
-          <li>
-            <a href="https://github.com/Mark-Cooper-Janssen-Vooles/devops-webdev-guide">devops-webdev-guide</a>
-          </li>
-        </ul>
       </header>
     </div>
   );
