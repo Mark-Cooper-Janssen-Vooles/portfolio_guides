32af48fcc5de484e2e4b7dd2858b1ec0c068b6a0
Mark Janssen-vooles <mark.janssen-vooles@xero.com>
2022-10-01 17:58:43 +1000

    [MASTER] Add an animated model

---
+++ src/AnimationArea.js
@@ -0,0 +1,49 @@
+import React, { useRef, Suspense } from 'react';
+import { Html, RoundedBox, useGLTF } from '@react-three/drei';
+import { Canvas, useFrame } from '@react-three/fiber'
+import { Mesh } from 'three'
+
+const Lights = () => {
+  return (
+  <>
+    <ambientLight intensity={0.3} />
+    <directionalLight position={[10, 10, 5]} intensity={1} />
+    <directionalLight position={[0, 10, 0]} intensity={1.5} />
+    <spotLight intensity={1} position={[1000, 0, 0]} />
+  </>
+  )
+}
+
+const Model = () => {
+  const gltf = useGLTF('/scene.gltf', true)
+  return <primitive object={gltf.scene} dispose={null} />
+}
+
+const RenderedObject = () => {
+  const ref = useRef()
+  useFrame(() => {
+    ref.current.rotation.y += 0.001
+  })
+
+  return <mesh ref={ref} position={[0, -63, 0]}>
+    <Model />
+  </mesh>
+}
+
+const AnimationArea = () => {
+  return (
+  <Canvas
+    colorManagement
+    camera={{position:[0, 0, 150], fov: 70}}
+  >
+    <Lights />
+    <Suspense fallback={null}>
+      <RenderedObject />
+    
+    </Suspense>
+  </Canvas>
+  )
+}
+
+export default AnimationArea;
+
--- src/App.js
+++ src/App.js
@@ -1,34 +1,42 @@
 import './App.css';
+import AnimationArea from './AnimationArea';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <div className="Content-wrap">
-          <h1>markjanssen-webdev-portfolio</h1>
-          <div>
-            I'm an architectural graduate turned full stack web software developer based in Australia. Predominantly using Javascript/React, C#/.net and AWS. This space is to detail my ongoing learnings, inspired by https://roadmap.sh/.
+          <div className="First-column">
+            <h2 style={{marginTop: "0px", marginBottom: "0px"}}>markjanssen-webdev-learnings</h2>
+            <AnimationArea />
           </div>
-          <ul>
-            <li>
-              <a href="https://github.com/Mark-Cooper-Janssen-Vooles/frontend-webdev-guide">frontend-webdev-guide</a>
-            </li>
-            <li>
-              <a href="https://github.com/Mark-Cooper-Janssen-Vooles/backend-webdev-guide">backend-webdev-guide</a>
-            </li>
-            <li>
-              <a href="https://github.com/Mark-Cooper-Janssen-Vooles/devops-webdev-guide">devops-webdev-guide</a>
-            </li>
-          </ul>
-          <div>
-            {/* usable icons here: https://fontawesome.com/v4/icons/ */}
-            <a href="https://www.linkedin.com/in/mark-janssen-vooles-312a77187/">
-              <i class="fa fa-linkedin-square" aria-hidden="true" href="https://www.linkedin.com/in/mark-janssen-vooles-312a77187/"></i> 
-            </a>
-            &nbsp;
-            <a href="https://github.com/Mark-Cooper-Janssen-Vooles/">
-              <i class="fa fa-github-square" aria-hidden="true"></i>
-            </a>
+          <div className="Second-column">
+            <div>
+              <p style={{marginTop: "0px", fontSize: "12px"}}>
+                I'm an architectural graduate turned full stack web software developer based in Australia. Predominantly using Javascript/React, C#/.net and AWS. This space is to detail my ongoing learnings, inspired by https://roadmap.sh/.
+              </p>
+              <ul style={{paddingLeft: "0px"}}>
+                <li>
+                  <a style={{fontSize: "12px"}} href="https://github.com/Mark-Cooper-Janssen-Vooles/frontend-webdev-guide">frontend-webdev-guide</a>
+                </li>
+                <li>
+                  <a style={{fontSize: "12px"}} href="https://github.com/Mark-Cooper-Janssen-Vooles/backend-webdev-guide">backend-webdev-guide</a>
+                </li>
+                <li>
+                  <a style={{fontSize: "12px"}} href="https://github.com/Mark-Cooper-Janssen-Vooles/devops-webdev-guide">devops-webdev-guide</a>
+                </li>
+              </ul>
+            </div>
+            <div style={{marginBottom: "20px"}}>
+              {/* usable icons here: https://fontawesome.com/v4/icons/ */}
+              <a href="https://www.linkedin.com/in/mark-janssen-vooles-312a77187/">
+                <i className="fa fa-linkedin-square" aria-hidden="true" href="https://www.linkedin.com/in/mark-janssen-vooles-312a77187/"></i> 
+              </a>
+              &nbsp;
+              <a href="https://github.com/Mark-Cooper-Janssen-Vooles/">
+                <i className="fa fa-github-square" aria-hidden="true"></i>
+              </a>
+            </div>
           </div>
         </div>
       </header>
